0a51b783400f6e032e7abfc9bf2314a620fca9ca
Dylan K <drkunkel7@gmail.com>
2021-06-29 12:10:53 -0500

    fully functional

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,24 @@
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl =  document.querySelector("#username");
 
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    });
+};
 
 var getUserRepos =  function(user) {
     // format the github api url
@@ -86,5 +102,16 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = "";
+    }
+}
+
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
